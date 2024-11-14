30c5e98007695c7a6313f3a1cdaaf499d313c483
sandikrivec14 <sandikrivec14@gmail.com>
2024-11-14 20:58:43 +0000

    fix dark mode, mobile responsive

--- src/App.vue
+++ src/App.vue
@@ -5,53 +5,47 @@
 <template>
   <!-- ABOUT ME -->
   <div class="text-left font-sans">
-    <table class="border">
-      <tbody>
-        <tr>
-          <td class="relative align-top text-center text-nowrap">
-            <div style="background: #f4ece6;box-shadow: 0 1px 4px rgba(0, 0, 0, .6);">
-              <div class="p-16">
-                <img src="./assets/sandikrivec.webp" class="w-40 h-40 object-cover rounded-full" fetchpriority="high">
-                <br>
-                <h3>Sandi Krivec</h3>
-                <a href="mailto:sandi.krivec@gmail.com">sandi.krivec@gmail.com</a>
-                <br>
-                <br>
-                <div class="w-20 h-3 border-t-2 border-blue-500 m-auto"></div>
-                <br>
-                <p class="" style="letter-spacing:0.25em">Software Engineer</p>
-              </div>
-              <div class="bg-white w-[100%]">
-                <div class="py-4">
-                  <a class="px-3" href="https://linkedin.com/in/sandi-krivec-28428b65" target="_blank">LinkedIn</a>
-                  <a class="px-3" href="https://github.com/schnapi" target="_blank">GitHub</a>
-                  <a class="px-3" href="https://gitlab.com/sandi.krivec" target="_blank">GitLab</a>
-                </div>
-              </div>
-            </div>
-          </td>
-          <td class=" p-16">
-            <h1>Hi</h1>
-            <br>
-            <h4>Here's who I am and what I can do for you</h4>
-            <br>
-            <h6>Python expert specialized in:</h6>
-            <ul>
-              <li>- Software architecture</li>
-              <li>- Workflows automation (GitLab CI/CD, Jenkins)</li>
-              <li>- Web development (Django, Celery, Vue, Bokeh)</li>
-              <li>- Android development</li>
-              <li>- Databases: SQLite, PostgreSQL, Redis, LMDB</li>
-              <li>- Others: MQTT, WebSockets</li>
-            </ul>
-            <br>
-            I use my skills to help businesses optimize their workflows and achieve their goals. Feel free to browse
-            my portfolio and contact me to discuss how I can help you.
-          </td>
-        </tr>
-      </tbody>
-    </table>
-
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-0 border">
+      <div class="col-span-1 text-center text-nowrap relative bg-[#f4ece6] dark:bg-slate-800"
+        style="box-shadow: 0 1px 4px rgba(0, 0, 0, .6);">
+        <div class="p-16">
+          <img src="./assets/sandikrivec.webp" class="w-40 h-40 object-cover rounded-full" fetchpriority="high">
+          <br>
+          <h3>Sandi Krivec</h3>
+          <a href="mailto:sandi.krivec@gmail.com">sandi.krivec@gmail.com</a>
+          <br>
+          <br>
+          <div class="w-20 h-3 border-t-2 border-blue-500 m-auto"></div>
+          <br>
+          <p class="" style="letter-spacing:0.25em">Software Engineer</p>
+        </div>
+        <div class="bg-white w-[100%] absolute bottom-0">
+          <div class="py-4">
+            <a class="px-3" href="https://linkedin.com/in/sandi-krivec-28428b65" target="_blank">LinkedIn</a>
+            <a class="px-3" href="https://github.com/schnapi" target="_blank">GitHub</a>
+            <a class="px-3" href="https://gitlab.com/sandi.krivec" target="_blank">GitLab</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-span-1 p-16">
+        <h1>Hi</h1>
+        <br>
+        <h4>Here's who I am and what I can do for you</h4>
+        <br>
+        <h6>Python expert specialized in:</h6>
+        <ul>
+          <li>- Software architecture</li>
+          <li>- Workflows automation (GitLab CI/CD, Jenkins)</li>
+          <li>- Web development (Django, Celery, Vue, Bokeh)</li>
+          <li>- Android development</li>
+          <li>- Databases: SQLite, PostgreSQL, Redis, LMDB</li>
+          <li>- Others: MQTT, WebSockets</li>
+        </ul>
+        <br>
+        I use my skills to help businesses optimize their workflows and achieve their goals. Feel free to browse
+        my portfolio and contact me to discuss how I can help you.
+      </div>
+    </div>
   </div>
   <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
